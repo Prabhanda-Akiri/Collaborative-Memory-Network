9523e459fb16b3ec13d39a7b3b719682c18d0c74
Akiri Prabhanda <31247199+Prabhanda-Akiri@users.noreply.github.com>
2019-04-30 00:31:52 +0530

    Create CMN.py

---
+++ CMN.py
@@ -0,0 +1,286 @@
+import numpy as np
+# import theano.tensor as T
+import keras
+from keras import backend as K
+from keras import initializers
+from keras.models import Sequential, Model, load_model, save_model
+from keras.layers.core import Dense, Lambda, Activation
+from keras.layers import Embedding, Input, Dense, Reshape, Flatten, Dropout
+# from keras.layers import merge, Merge
+from keras.layers.merge import multiply
+from keras.layers import concatenate
+from keras.optimizers import Adagrad, Adam, SGD, RMSprop
+from keras.regularizers import l1, l2, l1_l2
+# from Dataset import Dataset
+# from evaluate import evaluate_model
+from time import time
+import multiprocessing as mp
+import sys
+import math
+# import argparse
+import scipy.sparse as sp
+import heapq # for retrieval topK
+import multiprocessing
+#from numba import jit, autojit
+import warnings
+warnings.filterwarnings('ignore')
+
+class Dataset(object):
+    def __init__(self, path):
+        self.trainMatrix = self.load_rating_file_as_matrix(path + ".train.rating")
+        self.testRatings = self.load_rating_file_as_list(path + ".test.rating")
+        self.testNegatives = self.load_negative_file(path + ".test.negative")
+        assert len(self.testRatings) == len(self.testNegatives)
+        
+        self.num_users, self.num_items = self.trainMatrix.shape
+        
+    def load_rating_file_as_list(self, filename):
+        ratingList = []
+        with open(filename, "r") as f:
+            line = f.readline()
+            while line != None and line != "":
+                arr = line.split("\t")
+                user, item = int(arr[0]), int(arr[1])
+                ratingList.append([user, item])
+                line = f.readline()
+        return ratingList
+    
+    def load_negative_file(self, filename):
+        negativeList = []
+        with open(filename, "r") as f:
+            line = f.readline()
+            while line != None and line != "":
+                arr = line.split("\t")
+                negatives = []
+                for x in arr[1: ]:
+                    negatives.append(int(x))
+                negativeList.append(negatives)
+                line = f.readline()
+        return negativeList
+    
+    def load_rating_file_as_matrix(self, filename):
+        '''
+        Read .rating file and Return dok matrix.
+        The first line of .rating file is: num_users\t num_items
+        '''
+        # Get number of users and items
+        num_users, num_items = 0, 0
+        with open(filename, "r") as f:
+            line = f.readline()
+            while line != None and line != "":
+                arr = line.split("\t")
+                u, i = int(arr[0]), int(arr[1])
+                num_users = max(num_users, u)
+                num_items = max(num_items, i)
+                line = f.readline()
+        # Construct matrix
+        mat = sp.dok_matrix((num_users+1, num_items+1), dtype=np.float32)
+        with open(filename, "r") as f:
+            line = f.readline()
+            while line != None and line != "":
+                arr = line.split("\t")
+                user, item, rating = int(arr[0]), int(arr[1]), float(arr[2])
+                if (rating > 0):
+                    mat[user, item] = 1.0
+                line = f.readline()    
+        return mat
+# Global variables that are shared across processes
+_model = None
+_testRatings = None
+_testNegatives = None
+_K = None
+
+def evaluate_model(model, testRatings, testNegatives, K, num_thread):
+    """
+    Evaluate the performance (Hit_Ratio, NDCG) of top-K recommendation
+    Return: score of each test rating.
+    """
+    global _model
+    global _testRatings
+    global _testNegatives
+    global _K
+    _model = model
+    _testRatings = testRatings
+    _testNegatives = testNegatives
+    _K = K
+        
+    hits, ndcgs = [],[]
+    if(num_thread > 1): # Multi-thread
+        pool = multiprocessing.Pool(processes=num_thread)
+        res = pool.map(eval_one_rating, range(len(_testRatings)))
+        pool.close()
+        pool.join()
+        hits = [r[0] for r in res]
+        ndcgs = [r[1] for r in res]
+        return (hits, ndcgs)
+    # Single thread
+    for idx in range(len(_testRatings)):
+        (hr,ndcg) = eval_one_rating(idx)
+        hits.append(hr)
+        ndcgs.append(ndcg)      
+    return (hits, ndcgs)
+
+def eval_one_rating(idx):
+    rating = _testRatings[idx]
+    items = _testNegatives[idx]
+    u = rating[0]
+    gtItem = rating[1]
+    items.append(gtItem)
+    # Get prediction scores
+    map_item_score = {}
+    users = np.full(len(items), u, dtype = 'int32')
+    predictions = _model.predict([users, np.array(items)], 
+                                 batch_size=100, verbose=0)
+    for i in range(len(items)):
+        item = items[i]
+        map_item_score[item] = predictions[i]
+    items.pop()
+    
+    # Evaluate top rank list
+    ranklist = heapq.nlargest(_K, map_item_score, key=map_item_score.get)
+    hr = getHitRatio(ranklist, gtItem)
+    ndcg = getNDCG(ranklist, gtItem)
+    return (hr, ndcg)
+
+def getHitRatio(ranklist, gtItem):
+    for item in ranklist:
+        if item == gtItem:
+            return 1
+    return 0
+
+def getNDCG(ranklist, gtItem):
+    for i in range(len(ranklist)):
+        item = ranklist[i]
+        if item == gtItem:
+            return math.log(2) / math.log(i+2)
+    return 0
+def init_normal(shape, dtype=None):
+    return K.random_normal(shape, dtype=dtype)
+
+def get_model(num_users, num_items, mf_dim=10, layers=[10], reg_layers=[0], reg_mf=0):
+    assert len(layers) == len(reg_layers)
+    num_layer = len(layers) #Number of layers in the MLP
+    # Input variables
+    user_input = Input(shape=(1,), dtype='int32', name = 'user_input')
+    item_input = Input(shape=(1,), dtype='int32', name = 'item_input')
+    
+    # Embedding layer
+    MF_Embedding_User = Embedding(input_dim = num_users, output_dim = mf_dim, name = 'mf_embedding_user',
+                                  init = init_normal, W_regularizer = l2(reg_mf), input_length=1)
+    MF_Embedding_Item = Embedding(input_dim = num_items, output_dim = mf_dim, name = 'mf_embedding_item',
+                                  init = init_normal, W_regularizer = l2(reg_mf), input_length=1)   
+
+    MLP_Embedding_User = Embedding(input_dim = num_users, output_dim = int(layers[0]/2), name = "mlp_embedding_user",
+                                  init = init_normal, W_regularizer = l2(reg_layers[0]), input_length=1)
+    MLP_Embedding_Item = Embedding(input_dim = num_items, output_dim = int(layers[0]/2), name = 'mlp_embedding_item',
+                                  init = init_normal, W_regularizer = l2(reg_layers[0]), input_length=1)   
+    
+    # MF part
+    mf_user_latent = Flatten()(MF_Embedding_User(user_input))
+    mf_item_latent = Flatten()(MF_Embedding_Item(item_input))
+    mf_vector = multiply([mf_user_latent, mf_item_latent])
+    # MLP part 
+    mlp_user_latent = Flatten()(MLP_Embedding_User(user_input))
+    mlp_item_latent = Flatten()(MLP_Embedding_Item(item_input))
+    mlp_vector = concatenate([mlp_user_latent, mlp_item_latent])
+    for idx in range(1, num_layer):
+        layer = Dense(layers[idx], W_regularizer= l2(reg_layers[idx]), activation='relu', name="layer%d" %idx)
+        mlp_vector = layer(mlp_vector)
+
+    predict_vector = concatenate([mf_vector, mlp_vector])
+    
+    # Final prediction layer
+    prediction = Dense(1, activation='sigmoid', init='lecun_uniform', name = "prediction")(predict_vector)
+    
+    model = Model(input=[user_input, item_input], 
+                  output=prediction)
+    
+    return model
+  
+def get_train_instances(train, num_negatives):
+    user_input, item_input, labels = [],[],[]
+    num_users = train.shape[0]
+    for (u, i) in train.keys():
+        # positive instance
+        user_input.append(u)
+        item_input.append(i)
+        labels.append(1)
+        # negative instances
+        for t in range(num_negatives):
+            j = np.random.randint(num_items)
+            while (u, j) in train:
+                j = np.random.randint(num_items)
+            user_input.append(u)
+            item_input.append(j)
+            labels.append(0)
+    return user_input, item_input, labels
+# num_epochs = 1
+batch_size = 256
+mf_dim = 8
+layers = eval('[64,32,16,8]')
+reg_mf = 0
+reg_layers = eval('[0,0,0,0]')
+num_negatives = 4
+learning_rate = 0.001
+learner = 'adam'
+verbose = 1
+out_model = 1
+path = "/content/drive/My Drive/Data/"
+dataset = 'pinterest-20'
+topK = 10
+evaluation_threads = 1#mp.cpu_count()
+model_out_file = 'Pretrain/%s_CMN_%d_%s_%d.h5' %(dataset, mf_dim, layers, time())
+
+ # Loading data
+t1 = time()
+dataset = Dataset(path + dataset)
+train, testRatings, testNegatives = dataset.trainMatrix, dataset.testRatings, dataset.testNegatives
+num_users, num_items = train.shape
+print("Load data done [%.1f s]. #user=%d, #item=%d, #train=%d, #test=%d" 
+      %(time()-t1, num_users, num_items, train.nnz, len(testRatings)))
+# Build model
+model = get_model(num_users, num_items, mf_dim, layers, reg_layers, reg_mf)
+if learner.lower() == "adagrad": 
+    model.compile(optimizer=Adagrad(lr=learning_rate), loss='binary_crossentropy')
+elif learner.lower() == "rmsprop":
+    model.compile(optimizer=RMSprop(lr=learning_rate), loss='binary_crossentropy')
+elif learner.lower() == "adam":
+    model.compile(optimizer=Adam(lr=learning_rate), loss='binary_crossentropy')
+else:
+    model.compile(optimizer=SGD(lr=learning_rate), loss='binary_crossentropy')
+    
+# Init performance
+(hits, ndcgs) = evaluate_model(model, testRatings, testNegatives, topK, evaluation_threads)
+hr, ndcg = np.array(hits).mean(), np.array(ndcgs).mean()
+print('Init: HR = %.4f, NDCG = %.4f' % (hr, ndcg))
+best_hr, best_ndcg, best_iter = hr, ndcg, -1
+if out_model > 0:
+  model.save_weights(model_out_file, overwrite=True) 
+  
+# Training model
+num_epochs = 10
+for epoch in range(num_epochs):
+    t1 = time()
+    # Generate training instances
+    user_input, item_input, labels = get_train_instances(train, num_negatives)
+
+    # Training
+    hist = model.fit([np.array(user_input), np.array(item_input)], #input
+                     np.array(labels), # labels 
+                     batch_size=batch_size, nb_epoch=1, verbose=0, shuffle=True)
+    t2 = time()
+
+    # Evaluation
+    if epoch %verbose == 0:
+        (hits, ndcgs) = evaluate_model(model, testRatings, testNegatives, topK, evaluation_threads)
+        hr, ndcg, loss = np.array(hits).mean(), np.array(ndcgs).mean(), hist.history['loss'][0]
+        print('Iteration %d [%.1f s]: HR = %.4f, NDCG = %.4f, loss = %.4f [%.1f s]' 
+              % (epoch,  t2-t1, hr, ndcg, loss, time()-t2))
+        if hr > best_hr:
+            best_hr, best_ndcg, best_iter = hr, ndcg, epoch
+            if out_model > 0:
+                model.save_weights(model_out_file, overwrite=True)
+
+print("End. Best Iteration %d:  HR = %.4f, NDCG = %.4f. " %(best_iter, best_hr, best_ndcg))
+if out_model > 0:
+    print("The best CMN model is saved to %s" %(model_out_file))
